11888db8cda1ee31a5ac3aaa04210d393efae5da
Jehree <126540981+Jehree@users.noreply.github.com>
2025-12-16 19:07:52 -0500

    initial commit

---
+++ Mod.cs
@@ -0,0 +1,19 @@
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Logging;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
+
+namespace ModName;
+
+[Injectable(TypePriority = OnLoadOrder.PreSptModLoader + 1)]
+public class Mod(
+    ISptLogger<Mod> logger)
+    : IOnLoad
+{
+    public Task OnLoad()
+    {
+        
+        return Task.CompletedTask;
+    }
+}
